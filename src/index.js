39ec6383e0f41e936e7ae3ee7edd379d941db170
Andrew.I <andrewiheanacho811@gmail.com>
2024-11-11 10:53:05 -0600

    created the nav bar and added navigation for all pages required for the prototype

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import _ from 'lodash';
-
 import "./styles/styles.css";
+
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+
+function showPage(pageId) {
+  // Hide all sections
+  document.querySelectorAll("section").forEach(section => {
+    section.classList.remove("active");
+  });
+  
+  // If pageId is "Main", activate Main, Main2, and Contact sections
+  if (pageId === "Main" ) {
+      const sectionsToActivate = ["Main", "Main2", "contact"];
+      
+      sectionsToActivate.forEach(id => {
+          document.getElementById(id).classList.add("active");
+      });
+  } else {
+      // Otherwise, activate only the selected section
+      document.getElementById(pageId).classList.add("active");
+  }
+}
+
+// Expose the function globally
+window.showPage = showPage
